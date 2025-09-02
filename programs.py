33f48c0bca129906ad62ef01abda6805018f419d
Thanushri P <thanuuu.p@gmail.com>
2025-09-02 13:09:54 +0530

    Add files via upload
    A Python program to demonstrate different number data types(int, float, str, complex and bool).

---
+++ programs.py
@@ -0,0 +1,11 @@
+i = 7
+c = 24+8j
+f = 70.5
+s = "good morning" 
+bol = True
+
+print("The value of i is",i,"/n its type is:",type(i))
+print("The value of c is",c,"/n its type is:",type(c))
+print("The value of f is",f,"/n its type is:",type(f))
+print("The value of s is",s,"/n its type is:",type(s))
+print("The value of bol is",bol,"/n its type is:",type(bol))
