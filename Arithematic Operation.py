806f182af9c929d2b61a3b53571a52aa31b7a87e
Thanushri P <thanuuu.p@gmail.com>
2025-09-02 18:34:41 +0530

    Add files via upload
    Python program to perform basic arithmetic operations.

---
+++ Arithematic Operation.py
@@ -0,0 +1,11 @@
+a = 5 ; b = 7
+
+print("Addition of a;", a,"&b;", b,"is;",a+b)
+print("Subtraction of a;", a,"&b;", b,"is;",a-b)
+print("Multiplication of a;", a,"&b;", b,"is;",a*b)
+print("Division of a;", a,"&b;", b,"is;",a/b)
+print("Floor division of a;", a,"&b;", b,"is;",a//b)
+print("Module of a;", a,"&b;", b,"is;",a%b)
+print("Exponent of a;", a,"&b;", b,"is;",a**b)
+
+
